0977a628d7f9756542f6dc7750bac00d5f28e803
Eline31 <eline.chambon@gmail.com>
2023-06-22 21:17:14 +0200

    test experience display

--- src/Components/Carrousel/Carrousel.jsx
+++ src/Components/Carrousel/Carrousel.jsx
@@ -1,10 +1,7 @@
 import Experience from "../Experience-Card/Experience.jsx"
+import experiences from "../../Data/experiences.json"
 
 export default function Carrousel() {
-  return (
-    <>
-      <div>Carrousel</div>
-      <Experience />
-    </>
-  )
+  //   return <>{experiences && experiences.map((experience) => <Experience />)}</>
+  return <Experience />
 }
