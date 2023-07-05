da45c4a9f2bf3fa7bb5c586a00e507b915e977cd
Eline31 <eline.chambon@gmail.com>
2023-07-05 22:56:24 +0200

    Contact page OK

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,5 +1,32 @@
 import "./Contact.scss"
+import email from "../../Assets/mail.png"
+import phone from "../../Assets/smartphone-call.png"
+import home from "../../Assets/direction-sign.png"
+import generationy from "../../Assets/generationy.svg"
 
 export default function Contact() {
-  return <div>Contact</div>
+  return (
+    <div className="contact-wrapper">
+      <div className="item">
+        <img src={email} alt="icône email" />
+        <p className="item__title">Email</p>
+        <p className="item__content">eline.chambon@gmail.com</p>
+      </div>
+      <div className="item">
+        <img src={phone} alt="icône smartphone" />
+        <p className="item__title">Téléphone</p>
+        <p className="item__content">06.89.89.79.52</p>
+      </div>
+      <div className="item">
+        <img src={home} alt="icône panneau de direction" />
+        <p className="item__title">Adresse</p>
+        <p className="item__content">8, rue de Suez - 75018</p>
+      </div>
+      <div className="item">
+        <img src={generationy} alt="icône de la génération Y" />
+        <p className="item__title">Âge</p>
+        <p className="item__content">31 ans</p>
+      </div>
+    </div>
+  )
 }
