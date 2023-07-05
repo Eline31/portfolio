11248f5521c711d519db7485927be668132b0609
Eline31 <eline.chambon@gmail.com>
2023-07-05 20:44:35 +0200

    abilities OK

---
+++ src/Components/Abilities/Abilities.jsx
@@ -0,0 +1,43 @@
+import "./Abilities.scss"
+import languages from "../../Assets/speak.png"
+import design from "../../Assets/graphic-design.png"
+import os from "../../Assets/open-source.png"
+import coding from "../../Assets/web-programming.png"
+
+export default function Abilities() {
+  return (
+    <div className="abilities-wrapper">
+      <h2>Compétences</h2>
+      <div className="abilities">
+        <div className="ability">
+          <img src={languages} alt="icône langage" />
+          <p className="ability__title">Langues</p>
+          <p className="ability__description">
+            Français (langue natale), Anglais (courant)
+          </p>
+        </div>
+        <div className="ability">
+          <img src={design} alt="icône design" />
+          <p className="ability__title">Conception graphique</p>
+          <p className="ability__description">
+            Indesign, Photoshop, Illustrator, Figma
+          </p>
+        </div>
+        <div className="ability">
+          <img src={os} alt="icône open source" />
+          <p className="ability__title">Outils Open Source</p>
+          <p className="ability__description">
+            Github, Wiki, CodiMD, Nextcloud
+          </p>
+        </div>
+        <div className="ability">
+          <img src={coding} alt="icône développement web" />
+          <p className="ability__title">Développement web</p>
+          <p className="ability__description">
+            JavaScript, Node.JS, Express, MongoDB, API Rest, REACT
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/Pages/CV/CV.jsx
+++ src/Pages/CV/CV.jsx
@@ -1,17 +1,18 @@
 import "./CV.scss"
 import Carrousel from "../../Components/Carrousel/Carrousel"
 import Diploma from "../../Components/Diploma-Card/Diploma-Card"
+import Abilities from "../../Components/Abilities/Abilities"
 
 export default function CV() {
   return (
     <div className="cv-wrapper">
       <h1>Cheffe de projets numériques</h1>
       <div className="contentContainer">
-        <Carrousel />
-        {/* <div className="diplomas"> */}
-        <Diploma />
-        {/* </div> */}
-        {/* <div className="abilities"></div> */}
+        <div className="part1">
+          <Carrousel />
+          <Diploma />
+        </div>
+        <Abilities />
       </div>
     </div>
   )
