07802633dadf8425068e17b50831e42e7f54f4f4
Eline31 <eline.chambon@gmail.com>
2023-09-13 17:47:31 +0200

    title added contact

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -6,26 +6,29 @@ import generationy from "../../Assets/generationy.svg"
 
 export default function Contact() {
   return (
-    <div className="contact-wrapper">
-      <div className="item">
-        <img src={email} alt="icône email" />
-        <p className="item__title">Email</p>
-        <p className="item__content">eline.chambon@gmail.com</p>
-      </div>
-      <div className="item">
-        <img src={phone} alt="icône smartphone" />
-        <p className="item__title">Téléphone</p>
-        <p className="item__content">06.89.89.79.52</p>
-      </div>
-      <div className="item">
-        <img src={home} alt="icône panneau de direction" />
-        <p className="item__title">Adresse</p>
-        <p className="item__content">8, rue de Suez - 75018</p>
-      </div>
-      <div className="item">
-        <img src={generationy} alt="icône de la génération Y" />
-        <p className="item__title">Âge</p>
-        <p className="item__content">31 ans</p>
+    <div className="contact">
+      <h1>Contact</h1>
+      <div className="contact-wrapper">
+        <div className="item">
+          <img src={email} alt="icône email" />
+          <p className="item__title">Email</p>
+          <p className="item__content">eline.chambon@gmail.com</p>
+        </div>
+        <div className="item">
+          <img src={phone} alt="icône smartphone" />
+          <p className="item__title">Téléphone</p>
+          <p className="item__content">06.89.89.79.52</p>
+        </div>
+        <div className="item">
+          <img src={home} alt="icône panneau de direction" />
+          <p className="item__title">Adresse</p>
+          <p className="item__content">8, rue de Suez - 75018</p>
+        </div>
+        <div className="item">
+          <img src={generationy} alt="icône de la génération Y" />
+          <p className="item__title">Âge</p>
+          <p className="item__content">31 ans</p>
+        </div>
       </div>
     </div>
   )
