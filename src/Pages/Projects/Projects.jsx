991258b7b608f37bceed2772d1ce18ed64c795ae
Eline31 <eline.chambon@gmail.com>
2023-10-23 14:01:50 +0200

    add loader projects page

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,26 +1,48 @@
 import "./Projects.scss"
 import Project from "../../Components/Project-Card/Project-Card"
 import projects from "../../Data/projects.json"
+import { useState, useEffect } from "react"
+import { Loader } from "../../Utils/Style/Atoms.jsx"
 
 export default function Projects() {
+  //useState du loader
+  const [isLoading, setIsLoading] = useState(false)
+
+  //Vérification du chargement des données
+  useEffect(() => {
+    if (projects !== undefined) {
+      setIsLoading(false)
+    } else {
+      setIsLoading(true)
+    }
+  }, [])
+
   return (
-    <div className="page-content">
-      <h1>Mes projets</h1>
-      <div className="projects-wrapper">
-        {projects?.map((project) => (
-          <Project
-            key={project.id}
-            picture={project.image}
-            name={project.name}
-            field={project.field}
-            purpose={project.purpose}
-            description={project.description}
-            firm={project.firm}
-            knowledges={project.knowledges}
-            link={project.link}
-          />
-        ))}
-      </div>
-    </div>
+    <>
+      {isLoading ? (
+        <div className="LoaderWrapper">
+          <Loader />
+        </div>
+      ) : (
+        <div className="page-content">
+          <h1>Mes projets</h1>
+          <div className="projects-wrapper">
+            {projects?.map((project) => (
+              <Project
+                key={project.id}
+                picture={project.image}
+                name={project.name}
+                field={project.field}
+                purpose={project.purpose}
+                description={project.description}
+                firm={project.firm}
+                knowledges={project.knowledges}
+                link={project.link}
+              />
+            ))}
+          </div>
+        </div>
+      )}
+    </>
   )
 }
