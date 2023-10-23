82d8bb383be6be69746648a628dfd32e21699ea2
Eline31 <eline.chambon@gmail.com>
2023-10-23 14:20:58 +0200

    update experiences

--- src/Pages/CV/CV.jsx
+++ src/Pages/CV/CV.jsx
@@ -3,7 +3,6 @@ import Carrousel from "../../Components/Carrousel/Carrousel"
 import Diploma from "../../Components/Diploma-Card/Diploma-Card"
 import Abilities from "../../Components/Abilities/Abilities"
 import experiences from "../../Data/experiences.json"
-import experiencespro from "../../Data/experiencespro.json"
 
 export default function CV() {
   return (
@@ -14,11 +13,9 @@ export default function CV() {
           <div className="work">
             <h2>Expériences professionnelles</h2>
             <Carrousel
-              data={experiencespro}
-              key={`${experiencespro.firm}-${experiencespro.title}`}
+              data={experiences}
+              key={`${experiences.firm}-${experiences.title}`}
             />
-            <h2>Emplois saisonniers et étudiants</h2>
-            <Carrousel data={experiences} key={`title-${experiences.title}`} />
           </div>
           <Diploma key={`${Diploma.place}-${Diploma.dates}`} />
         </div>
